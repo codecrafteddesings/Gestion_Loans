7a09a01ee57eb41495e23d6eec49815d67c939b7
CodeCrafted <codecrafteddesings@gmail.com>
2024-07-21 21:18:06 -0400

    Actualizar con los cambios recientes

--- loan-app/backend/models/Cliente.js
+++ loan-app/backend/models/Cliente.js
@@ -25,37 +25,3 @@ const ClienteSchema = new Schema({
 }, { timestamps: true });
 
 module.exports = mongoose.model('Cliente', ClienteSchema);
-
-
-/*const mongoose = require('mongoose');
-
-const ClienteSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: true,
-    },
-    email: {
-        type: String,
-        required: true,
-        unique: true,
-    },
-    phone: {
-        type: String,
-        required: true,
-    },
-    idNumber: {
-        type: String,
-        required: true,
-    },
-    address: {
-        type: String,
-        required: true,
-    },
-    createdAt: {
-        type: Date,
-        default: Date.now,
-    },
-});
-
-module.exports = mongoose.model('Cliente', ClienteSchema);
-*/
